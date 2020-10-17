709ff4edb0d3eee92e9f7ed064493e977bc60392
Budi Darmawan <budymitman_77@yahoo.co.id>
2020-10-17 12:28:13 +0800

    commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 require('dotenv').config()
-const cors = require('cors')   
+const cors = require('cors')
 
 const planesRouter = require('./src/routes/planes')
 const profileRouter = require('./src/routes/profile')
@@ -15,30 +15,30 @@ app.use('/profile', profileRouter)
 app.use('/assets', express.static('./assets'))
 
 app.use((request, response, next) => {
-    response.header('Access-Control-Allow-Origin', '*')
-    response.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept',
-      'Authorization')
-    next()
-  })
+  response.header('Access-Control-Allow-Origin', '*')
+  response.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+    'Authorization')
+  next()
+})
 
 app.get('/', (request, response) => {
-    response.send({
-        success: true,
-        message: 'Server Back End Ankasa Ticketing'
-    })
+  response.send({
+    success: true,
+    message: 'Server Back End Ankasa Ticketing'
+  })
 })
 
 app.use((request, response, next) => {
-    response.header('Access-Control-Allow-Origin', '*')
-    response.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept',
-      'Authorization')
-    next()
-  })
+  response.header('Access-Control-Allow-Origin', '*')
+  response.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+    'Authorization')
+  next()
+})
 
 app.listen(process.env.PORT, () => {
-    console.log(`Listening to port ${process.env.PORT}`)
+  console.log(`Listening to port ${process.env.PORT}`)
 })
--- src/controllers/planes.js
+++ src/controllers/planes.js
@@ -1,133 +1,135 @@
+/* eslint-disable camelcase */
+
 const {
-    getPlanesDataModel,
-    createPlaneDataModel,
-    putPlaneDataModel,
-    getPlanesDataByIdModel,
-    deletePlaneDataModel
+  getPlanesDataModel,
+  createPlaneDataModel,
+  putPlaneDataModel,
+  getPlanesDataByIdModel,
+  deletePlaneDataModel
 } = require('../models/planes')
 
 module.exports = {
-    getPlanesData: (req, res) => {
-        let { search, page, limit } = req.query
-        let searchKey = ''
-        let searchValue = ''
-        if (typeof search === 'object') {
-          searchKey = Object.keys(search)[0]
-          searchValue = Object.values(search)[0]
-        } else {
-          searchKey = 'plane_name'
-          searchValue = search || ''
-        }
-        !limit ? limit = 20 : limit = parseInt(limit)
-        !page ? page = 1 : page = parseInt(page)
-        const offset = (page - 1) * limit
-        getPlanesDataModel(searchKey, searchValue, limit, offset, result => {
-          if (result.length) {
-            res.status(200).send({
-              success: true,
-              message: 'list planes',
-              data: result
-            })
-          } else {
-            console.log(result)
-            res.status(404).send({
-              success: false,
-              message: 'there is no item on list'
-            })
-          }
+  getPlanesData: (req, res) => {
+    let { search, page, limit } = req.query
+    let searchKey = ''
+    let searchValue = ''
+    if (typeof search === 'object') {
+      searchKey = Object.keys(search)[0]
+      searchValue = Object.values(search)[0]
+    } else {
+      searchKey = 'plane_name'
+      searchValue = search || ''
+    }
+    !limit ? limit = 20 : limit = parseInt(limit)
+    !page ? page = 1 : page = parseInt(page)
+    const offset = (page - 1) * limit
+    getPlanesDataModel(searchKey, searchValue, limit, offset, result => {
+      if (result.length) {
+        res.status(200).send({
+          success: true,
+          message: 'list planes',
+          data: result
         })
-    },
-    getPlaneDataById: (req, res) => {
-        const { id } = req.params
-        getPlanesDataByIdModel(id, result => {
-          if (result.length) {
-            res.status(200).send({
-              success: true,
-              message: `Data plane with id = ${id}`,
-              data: result[0]
-            })
-          } else {
-            res.status(404).send({
-              success: false,
-              message: `Data plane with id = ${id} was not found!`
-            })
-          }
+      } else {
+        console.log(result)
+        res.status(404).send({
+          success: false,
+          message: 'there is no item on list'
         })
-    },
-    createPlaneData: (req, res) => {
-        const { plane_name } = req.body
-        const image = typeof req.file === 'undefined' ? '' : req.file.filename
-        if (plane_name) {
-          createPlaneDataModel(plane_name, image, result => {
-            res.status(200).send({
-              success: true,
-              message: 'plane has been created'
-            })
-          })
-        } else {
-          res.status(400).send({
-            success: false,
-            message: 'the field plane name must be filled!'
-          })
-        }
-    },
-    putPlaneData: (req, res) => {
-        const { plane_name } = req.body
-        const id = req.params.id
-        const image = typeof req.file === 'undefined' ? '' : req.file.filename
-        if (plane_name) {
-          getPlanesDataByIdModel(id, result => {
-            if (result.length) {
-              putPlaneDataModel(id, plane_name, image, result => {
-                if (result.affectedRows) {
-                  res.status(200).send({
-                    success: true,
-                    message: `plane with id ${id} has been updated`
-                  })
-                } else {
-                  res.status(400).send({
-                    success: false,
-                    message: 'failed to update data'
-                  })
-                }
+      }
+    })
+  },
+  getPlaneDataById: (req, res) => {
+    const { id } = req.params
+    getPlanesDataByIdModel(id, result => {
+      if (result.length) {
+        res.status(200).send({
+          success: true,
+          message: `Data plane with id = ${id}`,
+          data: result[0]
+        })
+      } else {
+        res.status(404).send({
+          success: false,
+          message: `Data plane with id = ${id} was not found!`
+        })
+      }
+    })
+  },
+  createPlaneData: (req, res) => {
+    const { plane_name } = req.body
+    const image = typeof req.file === 'undefined' ? '' : req.file.filename
+    if (plane_name) {
+      createPlaneDataModel(plane_name, image, result => {
+        res.status(200).send({
+          success: true,
+          message: 'plane has been created'
+        })
+      })
+    } else {
+      res.status(400).send({
+        success: false,
+        message: 'the field plane name must be filled!'
+      })
+    }
+  },
+  putPlaneData: (req, res) => {
+    const { plane_name } = req.body
+    const id = req.params.id
+    const image = typeof req.file === 'undefined' ? '' : req.file.filename
+    if (plane_name) {
+      getPlanesDataByIdModel(id, result => {
+        if (result.length) {
+          putPlaneDataModel(id, plane_name, image, result => {
+            if (result.affectedRows) {
+              res.status(200).send({
+                success: true,
+                message: `plane with id ${id} has been updated`
               })
             } else {
-              res.status(404).send({
+              res.status(400).send({
                 success: false,
-                message: `plane with id ${id} is not found!`
+                message: 'failed to update data'
               })
             }
           })
         } else {
-          res.status(400).send({
+          res.status(404).send({
             success: false,
-            message: 'plane name must be filled!'
+            message: `plane with id ${id} is not found!`
           })
         }
-      },
-      deletePlaneData: (req, res) => {
-        const { id } = req.params
-        getPlanesDataByIdModel(id, result => {
-          if (result.length) {
-            deletePlaneDataModel(id, result => {
-              if (result.affectedRows) {
-                res.status(200).send({
-                  success: true,
-                  message: `Item plane with id = ${id} has been deleted`
-                })
-              } else {
-                res.status(400).send({
-                  success: false,
-                  message: 'Failed to delete'
-                })
-              }
+      })
+    } else {
+      res.status(400).send({
+        success: false,
+        message: 'plane name must be filled!'
+      })
+    }
+  },
+  deletePlaneData: (req, res) => {
+    const { id } = req.params
+    getPlanesDataByIdModel(id, result => {
+      if (result.length) {
+        deletePlaneDataModel(id, result => {
+          if (result.affectedRows) {
+            res.status(200).send({
+              success: true,
+              message: `Item plane with id = ${id} has been deleted`
             })
           } else {
-            res.status(404).send({
+            res.status(400).send({
               success: false,
-              message: 'Data plane was not found!'
+              message: 'Failed to delete'
             })
           }
         })
+      } else {
+        res.status(404).send({
+          success: false,
+          message: 'Data plane was not found!'
+        })
       }
+    })
+  }
 }
